1655af92c8bdec5f272154c8578e9a3f485dc80d
Emmanuel Boudrant <emmanuel.boudrant@gmail.com>
2012-01-01 12:01:01 +0800

    clean code (jslint)

--- test/test_grabit.js
+++ test/test_grabit.js
@@ -1,20 +1,17 @@
-
 /*!
  * GrabIt
  * Copyright(c) 2012 ebtokyo <emmanuel.boudrant@gmail.com>
  */
- 
 var grabit = require('../lib/grabit.js');
 var http = require('http');
-
 /**
  * grabitDownload test require an access to https://ajax.googleapis.com
  * test file used is jquery.min.js
  */
 exports.grabitDownload = function(test) {
-    var stringUrl = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
-    var o = new grabit.GUrl(stringUrl);
-    var request = http.request(o.getOptions());
+    var stringUrl = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
+        o = new grabit.GUrl(stringUrl),
+        request = http.request(o.getOptions());
     request.end();
     request.on('response', function(response) {
         response.setEncoding('utf8');
@@ -22,45 +19,39 @@ exports.grabitDownload = function(test) {
         test.done();
     });
 };
-
-exports.grabitFromSimpleUrl = function (test) {
-    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex.aspx';
-    var o = new grabit.GUrl(stringUrl);
+exports.grabitFromSimpleUrl = function(test) {
+    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex.aspx',
+        o = new grabit.GUrl(stringUrl);
     test.equal(o.url.href, stringUrl);
     test.done();
 };
-
-exports.grabitFromUrlWithoutExtension = function (test) {
-    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex';
-    var o = new grabit.GUrl(stringUrl);
+exports.grabitFromUrlWithoutExtension = function(test) {
+    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex',
+        o = new grabit.GUrl(stringUrl);
     test.equal(o.url.href, stringUrl);
     test.done();
 };
-
-exports.grabitFromUrlWithPort = function (test) {
-    var stringUrl = 'http://www.cambiaresearch.com:80/snippets/csharp/regex/uri_regex';
-    var o = new grabit.GUrl(stringUrl);
+exports.grabitFromUrlWithPort = function(test) {
+    var stringUrl = 'http://www.cambiaresearch.com:80/snippets/csharp/regex/uri_regex',
+        o = new grabit.GUrl(stringUrl);
     test.equal(o.url.href, stringUrl);
     test.done();
 };
-
-exports.grabitFromUrlWithHash = function (test) {
-    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex.aspx#hash';
-    var o = new grabit.GUrl(stringUrl);
+exports.grabitFromUrlWithHash = function(test) {
+    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex.aspx#hash',
+        o = new grabit.GUrl(stringUrl);
     test.equal(o.url.href, stringUrl);
     test.done();
 };
-
-exports.grabitFromUrlWithQuery = function (test) {
-    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex.aspx?q=t&a=b';
-    var o = new grabit.GUrl(stringUrl);
+exports.grabitFromUrlWithQuery = function(test) {
+    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex.aspx?q=t&a=b',
+        o = new grabit.GUrl(stringUrl);
     test.equal(o.url.href, stringUrl);
     test.done();
 };
-
-exports.grabitFromUrlWithQueryAndHash = function (test) {
-    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex.aspx?q=t&a=b#hash';
-    var o = new grabit.GUrl(stringUrl);
+exports.grabitFromUrlWithQueryAndHash = function(test) {
+    var stringUrl = 'http://www.cambiaresearch.com/snippets/csharp/regex/uri_regex.aspx?q=t&a=b#hash',
+        o = new grabit.GUrl(stringUrl);
     test.equal(o.url.href, stringUrl);
     test.done();
 };
--- web.js
+++ web.js
@@ -1,13 +1,20 @@
+/*!
+ * GrabIt
+ * Copyright(c) 2012 ebtokyo <emmanuel.boudrant@gmail.com>
+ */
 var express = require('express');
 var grabit = require("./lib/grabit.js");
 var app = express.createServer(express.logger());
-
+/**
+ * ExpressJS /grabit mapping
+ */
 app.get('/grabit', function(req, res) {
     if (req.query.src) {
         console.log('new request on ' + req.query.src + ' ...');
         var grabIt = new grabit.GUrl(req.query.src);
         grabIt.transfertTo(res);
-    } else {
+    }
+    else {
         res.writeHead(200, {
             'Content-Type': 'text/html',
             'Cache-control': 'no-store'
@@ -15,8 +22,10 @@ app.get('/grabit', function(req, res) {
         res.end('not found');
     }
 });
-
+/**
+ * ExpressJS launcher
+ */
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+    console.log('Listening on ' + port);
 });
